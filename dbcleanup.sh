0a793488fa48f82638bf162b161ce89ca2249353
Viacheslav Safronov <31385582+SlavaSafronov@users.noreply.github.com>
2018-10-13 17:29:36 +0300

    Update dbcleanup.sh

--- dbcleanup.sh
+++ dbcleanup.sh
@@ -2,8 +2,15 @@
 # Title: 		cleanup database
 # created by: 	Slava Safronov
 
-# location variables
-work_folder='/tmp/dbcleanup/'
+# create work directory and subdirectories
+        work_folder='/tmp/dbcleanup/'
+
+        mkdir -p $work_folder
+        chmod -R 0777 $work_folder
+
+        #define log
+        log_file="$work_folder/dbcleanup.log"
+        > ${log_file}
 
 
 # functions
@@ -14,3 +21,7 @@ work_folder='/tmp/dbcleanup/'
 
 
 # main	
+today=$(date)
+echo "DBCleanup log: " > ${log_file}
+echo "   Started at: $today" >> ${log_file}
+echo " =======================================================" >> ${log_file}
