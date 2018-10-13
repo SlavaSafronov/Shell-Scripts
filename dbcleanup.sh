4ffa97f3dddfa5cd1cb2374bbcd1ee8edcec6986
Viacheslav Safronov <31385582+SlavaSafronov@users.noreply.github.com>
2018-10-13 17:16:53 +0300

    Update dbcleanup.sh

--- dbcleanup.sh
+++ dbcleanup.sh
@@ -1,1 +1,16 @@
 #!/bin/sh
+# Title: 		cleanup database
+# created by: 	Slava Safronov
+
+# location variables
+work_folder='/tmp/dbcleanup/'
+
+
+# functions
+	delete_tickets(){
+    
+	}
+
+
+
+# main	
