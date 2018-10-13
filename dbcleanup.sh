1ceda0f0181ca0100a3278b0036206302bbf570f
Viacheslav Safronov <31385582+SlavaSafronov@users.noreply.github.com>
2018-10-13 17:40:54 +0300

    Update dbcleanup.sh

--- dbcleanup.sh
+++ dbcleanup.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
-# Title: 		cleanup database
+# Title: 	cleanup database
 # created by: 	Slava Safronov
 
 # create work directory and subdirectories
         work_folder='/tmp/dbcleanup/'
-
         mkdir -p $work_folder
         chmod -R 0777 $work_folder
 
@@ -12,6 +11,16 @@
         log_file="$work_folder/dbcleanup.log"
         > ${log_file}
 
+# define API connection credentials
+	URL='https://testent18.sysaidit.com'
+	API_ACCOUNT_ID='testent18'
+	API_USER=''
+	API_PASS=''
+	
+	API_LOGIN_URL=${URL}/api/v1/login -d '{"user_name":"$API_USER","account_id":"$API_ACCOUNT_ID","password":"$API_PASS"}'
+	
+	
+	
 
 # functions
 	delete_tickets(){
